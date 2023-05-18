84c55e4877442d48b87a5b458dc2e79aa266659c
sokhiennn <22013179@myrp.edu.sg>
2023-05-18 11:43:56 +0800

    Demo Riddle 100% completed

--- app/src/main/java/sg/edu/rp/c346/id22013179/demoriddle/AnswerActivity1.java
+++ app/src/main/java/sg/edu/rp/c346/id22013179/demoriddle/AnswerActivity1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class AnswerActivity1 extends AppCompatActivity {
@@ -15,6 +16,8 @@ public class AnswerActivity1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer1);
 
+        Log.d("AnswerActivity1", "onCreate() called.");
+
         tvAnswer = findViewById(R.id.textView);
         //tvAnswer.setText ("In Second Activity");
         Intent intentReceived = getIntent();
@@ -22,4 +25,36 @@ public class AnswerActivity1 extends AppCompatActivity {
         tvAnswer.setText(questionsSelected + " answer is: Queue");
 
     }
+    @Override
+    protected void onStart() {
+        Log.d("AnswerActivity1", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("AnswerActivity1", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("AnswerActivity1", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("AnswerActivity1", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("AnswerActivity1", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("AnswerActivity1", "onRestart() called.");
+        super.onRestart();
+    }
 }
+
+
